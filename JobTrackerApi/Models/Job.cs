6e32759febc66efdbd21405b749bcb42fda8941f
Rachid Aifar <rachid.aifar24@gmail.com>
2026-05-15 11:20:26 +0200

    Initial ASP.NET Core Web API project

---
+++ JobTrackerApi/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace JobTrackerApi.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Company { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public string Status { get; set; } = "Applied";
+        public DateTime AppliedDate { get; set; } = DateTime.UtcNow;
+    }
+}
