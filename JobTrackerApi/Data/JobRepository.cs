abf9550168ccecad5ba0ea15e3bd8553a1b5f706
Rachid Aifar <rachid.aifar24@gmail.com>
2026-05-15 11:39:16 +0200

    Add testing in-memory Job repo

---
+++ JobTrackerApi/Data/JobRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobTrackerApi.Models;
+
+namespace JobTrackerApi.Data
+{
+    public class JobRepository
+    {
+        public static List<Job> Jobs = new List<Job>()
+        {
+        new Job {Id = 1, Company = "Google", Position = "Backend Dev" },
+        new Job {Id = 2, Company = "Microsoft", Position = "Cloud Engineer" },
+        };
+    }
+}
