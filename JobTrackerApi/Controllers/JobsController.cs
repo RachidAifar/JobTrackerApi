4af53d1217a4004bb680c4beebf9c90b855cc76d
Rachid Aifar <rachid.aifar24@gmail.com>
2026-05-15 11:52:34 +0200

    Add Jobs controller with Get endpoint

---
+++ JobTrackerApi/Controllers/JobsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JobTrackerApi.Data;
+using JobTrackerApi.Models;
+
+namespace JobTrackerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JobsController : ControllerBase
+    {
+        //GET: api/jobs
+        [HttpGet]
+        public ActionResult<List<Job>> GetAllJobs()
+        {
+            return Ok(JobRepository.Jobs);
+        }
+    }
+}
