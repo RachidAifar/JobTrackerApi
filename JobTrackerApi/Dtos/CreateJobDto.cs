c732f24e5b6716b8e713f7a9de449d331bcb3d14
Rachid Aifar <rachid.aifar24@gmail.com>
2026-05-31 19:55:10 +0200

    Add authentication, job management, and statistics API

--- JobTrackerApi/Dtos/CreateJobDto.cs
+++ JobTrackerApi/Dtos/CreateJobDto.cs
@@ -1,8 +1,15 @@
-﻿namespace JobTrackerApi.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTrackerApi.Dtos
 {
     public class CreateJobDto
     {
+        [Required]
+        [StringLength(100)]
         public string Company { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
         public string Position { get; set; } = string.Empty;
     }
 }
